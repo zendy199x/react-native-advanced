ef4184fa05f038b9bb2fceaecd124c09fa8a0299
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-01 15:05:49 +0700

    Layout

--- style/App.js
+++ style/App.js
@@ -5,29 +5,21 @@ import {View, StyleSheet, Dimensions} from 'react-native';
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'red',
-      }}>
-      <View
-        style={{
-          flex: 1,
-          // height: height / 4,
-          backgroundColor: 'green',
-        }}></View>
-      <View
-        style={{
-          flex: 2,
-          // height: height / 2,
-          backgroundColor: 'blue',
-        }}></View>
-      <View
-        style={{
-          flex: 1,
-          // height: height / 4,
-          backgroundColor: 'yellow',
-        }}></View>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+      </View>
+      <View style={{flex: 2, flexDirection: 'row'}}>
+        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+        <View style={{flex: 1, backgroundColor: 'yellow'}}></View>
+        <View style={{flex: 2, backgroundColor: 'green'}}></View>
+      </View>
+      <View style={{flex: 3}}>
+        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+        <View style={{flex: 2, backgroundColor: 'blue'}}></View>
+      </View>
     </View>
   );
 };
