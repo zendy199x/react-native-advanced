b768693546d9b7c361d518c9dc10c80d94888d81
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-05-01 14:55:13 +0700

    FlexBox

--- style/App.js
+++ style/App.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
+
+// const {height} = Dimensions.get('window');
 
 const App = () => {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row'
+        backgroundColor: 'red',
       }}>
       <View
-        //inline style
-        style={styles.redSquare}></View>
-      <View style={styles.greenSquare}></View>
+        style={{
+          flex: 1,
+          // height: height / 4,
+          backgroundColor: 'green',
+        }}></View>
+      <View
+        style={{
+          flex: 2,
+          // height: height / 2,
+          backgroundColor: 'blue',
+        }}></View>
+      <View
+        style={{
+          flex: 1,
+          // height: height / 4,
+          backgroundColor: 'yellow',
+        }}></View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  redSquare: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'red',
-  },
-  greenSquare: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'green',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
